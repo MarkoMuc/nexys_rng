8ab303e9ff751de3f8135d55cb9f8e3831d7dfab
Marko Z. Muc <marko.muc12@gmail.com>
2024-03-06 17:56:48 +0100

    spi_init

---
+++ random_num_gen.srcs/sources_1/new/spi_master.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/06/2024 05:54:50 PM
+-- Design Name: 
+-- Module Name: spi_master - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity spi_master is
+    Port ( SPI_1 : in STD_LOGIC);
+end spi_master;
+
+architecture Behavioral of spi_master is
+
+begin
+
+
+end Behavioral;
